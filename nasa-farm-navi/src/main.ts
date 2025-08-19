7e81785449ef7b2409e8ea5a17799dc4e82c1f83
Yuri Ferreira Paulo <yuritec@outlook.com>
2025-08-18 23:26:01 -0300

    Update main.ts

--- nasa-farm-navi/src/main.ts
+++ nasa-farm-navi/src/main.ts
@@ -1,14 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-
-// Estilos globais (reset + utilitários que você já tem no main.css)
 import './assets/styles/main.css'
 
 const app = createApp(App)
-
-// Ativar o Pinia (store global)
 app.use(createPinia())
-
-// Montar a aplicação no index.html (#app)
 app.mount('#app')
